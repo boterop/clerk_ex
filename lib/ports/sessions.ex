e652df3995e0dae1b711402e002552e9cf63a291
Santiago Botero <98826652+boterop@users.noreply.github.com>
2025-05-12 15:57:18 -0500

    Sessions (#10)
    * Session port
    
    * Session adapter
    
    * Context and documentation

---
+++ lib/ports/sessions.ex
@@ -0,0 +1,16 @@
+defmodule Clerk.Ports.Sessions do
+  @moduledoc """
+  Behaviour that defines the expected functions for a Sessions adapter. 
+  """
+
+  alias Clerk.Shared.HandleResponse
+
+  @callback list(map()) :: HandleResponse.t()
+  @callback create(String.t()) :: HandleResponse.t()
+  @callback retrieve(String.t()) :: HandleResponse.t()
+  @callback refresh(String.t(), map()) :: HandleResponse.t()
+  @callback revoke(String.t()) :: HandleResponse.t()
+  @callback create_token(String.t(), integer() | nil) :: HandleResponse.t()
+  @callback create_from_jwt_template(String.t(), String.t(), integer() | nil) ::
+              HandleResponse.t()
+end
